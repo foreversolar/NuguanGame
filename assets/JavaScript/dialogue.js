85badb6816c997dfa83151516e7875f09cb23688
ygdddd <y921163534@163.com>
2020-05-04 11:40:36 +0800

    可以跳过对话，但是主页加载缓慢可能导致最后一句重复

--- assets/JavaScript/dialogue.js
+++ assets/JavaScript/dialogue.js
@@ -9,9 +9,6 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        Character_left:cc.Sprite,
-        Character_right:cc.Sprite,
-        Dialogue:cc.Sprite,
         DialogueList:cc.JsonAsset,
         text:cc.Label,
 
@@ -21,6 +18,7 @@ cc.Class({
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
+        cc.director.preloadScene('Game');
         //初始化云服务器
         wx.cloud.init({
             traceUser: true,
@@ -36,60 +34,74 @@ cc.Class({
     },
 
     start () {
+        var i = 0;
+        var j = 0;
+        var k = 0;
         var self = this;
-        var index = 0;
-        cc.director.preloadScene('Game'),
-        this.node.on('touchend',function(){
-            if(self.ifplaying){
-                self.ifplaying = 0;
-                self.Dialogue_continue(index);
-                if(index<self.DialogueList.json.StartDialogue.length){
-                    index++;
-                }
-            }
-        },this);
-    },
-    //update (dt) {},
-
-
-    Dialogue_continue:function(index){
         var textjson = this.DialogueList.json;
+        var str = textjson.StartDialogue[i];
+
         var Character_left = this.node.getChildByName("Character_left");
         var Character_right = this.node.getChildByName("Character_right");
         var Dialogue_L = this.node.getChildByName("dialogue_left");
         var Dialogue_R = this.node.getChildByName("dialogue_right");
-        if(index == textjson.StartDialogue.length)
-            cc.director.loadScene('Game');
-        else{
-            if(Character_left.opacity == 0&&Dialogue_L.opacity == 0&&index!=0){
-                Character_right.opacity = 0;
-                Dialogue_R.opacity = 0;
-                Character_left.opacity = 255;
-                Dialogue_L.opacity = 255;
-            }
-            else{
-                Character_right.opacity = 255;
-                Dialogue_R.opacity = 255;
-                Character_left.opacity = 0;
-                Dialogue_L.opacity = 0;
-            }
-            this.showText(textjson.StartDialogue[index]);
-        }
-    },
 
-    showText:function(str){
-        var index= 0;
         var interval = 0.05;
         var repeat=str.length-1;
         var delay = 0;
-        this.text.string = "";
-        this.schedule(function(){
-            this.text.string += str[index];
-            if((index-28)%29 == 0&&index!=0)
+
+        var nextDialogue = function(){
+            this.text.string += str[j];
+            if((j-28)%29 == 0&&j!=0)
                 this.text.string += '\n';
-            index++;
-            if(index == str.length-1)
+            j++;
+            if(j == str.length){
+                i++;
                 this.ifplaying = 1;
-        },interval,repeat,delay);  
+            }
+        };
+
+        this.node.on('touchend',function(){            
+            if(i >= textjson.StartDialogue.length-1)
+                cc.director.loadScene('Game');
+            else{
+                if(Character_left.opacity == 0&&Dialogue_L.opacity == 0&&k!=0){
+                    Character_right.opacity = 0;
+                    Dialogue_R.opacity = 0;
+                    Character_left.opacity = 255;
+                    Dialogue_L.opacity = 255;
+                }
+                else{
+                    Character_right.opacity = 255;
+                    Dialogue_R.opacity = 255;
+                    Character_left.opacity = 0;
+                    Dialogue_L.opacity = 0;
+                }
+            }
+            k++;
+            if(self.ifplaying){
+                self.ifplaying = 0;
+                self.text.string = "";
+                if(i < textjson.StartDialogue.length){
+                    str = textjson.StartDialogue[i];
+                    repeat=str.length-1;
+                }
+                self.schedule(nextDialogue,interval,repeat,delay);
+                j = 0;
+            }
+            else{
+                self.unschedule(nextDialogue);  
+                j = 0;
+                self.text.string = "";
+                i++;
+                if(i<self.DialogueList.json.StartDialogue.length){
+                    str = textjson.StartDialogue[i];
+                    repeat=str.length-1;
+                }
+                self.schedule(nextDialogue,interval,repeat,delay);
+            }
+            
+        },this);
     },
+    //update (dt) {},
 });

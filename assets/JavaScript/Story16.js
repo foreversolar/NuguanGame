c2253c6402fad093d71fe345eef1cbcc181b76f4
119991121 <1261223497@qq.com>
2020-06-02 23:09:24 +0800

    story 10 16 24

---
+++ assets/JavaScript/Story16.js
@@ -0,0 +1,123 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        me:cc.Node,
+        friend: cc.Node,
+        gugu: cc.Node,
+        fuzi:cc.Node,
+        playerName: cc.Label,
+        friendSay:cc.Label,
+        mySay: cc.Label,
+        guguSay: cc.Label,
+        fuziSay:cc.Label,
+        text: cc.JsonAsset,
+        liniang_haogandu: 3
+    },
+
+
+    onLoad () {
+        cc.director.preloadScene("Game");
+    },
+
+    start () {
+        this.gugu.opacity = 0;
+        this.me.opacity = 0;
+        this.friend.opacity = 0;
+        this.fuzi.opacity = 0;
+
+        var Dialogue;
+        var i = 1;
+        if (this.liniang_haogandu == 0) {
+            Dialogue = this.text.json.rounds16.dialogue1;
+            this.fuzi.opacity = 255;
+            this.fuziSay.string = Dialogue[0];
+            this.node.on('touchend', function () {
+                if (i > 4) {
+                    cc.director.loadScene("Game");
+                } else if (i == 1) {
+                    this.fuzi.opacity = 0;
+                    this.me.opacity = 255;
+                    this.mySay.string = Dialogue[i]
+                } else if (i == 2) {
+                    this.fuzi.opacity = 255;
+                    this.me.opacity = 0;
+                    this.fuziSay.string = Dialogue[i]
+                } else if (i == 3) {
+                    this.fuzi.opacity = 0;
+                    this.friend.opacity = 255;
+                    this.friendSay.string = Dialogue[i]
+                } else if (i == 4) {
+                    this.friend.opacity = 0;
+                    this.me.opacity = 255;
+                    this.mySay.string = Dialogue[i]
+                }
+
+                i++;
+            }, this)
+        } else if (this.liniang_haogandu == 1 || this.liniang_haogandu == 2) {
+            Dialogue = this.text.json.rounds16.dialogue2;
+            this.friend.opacity = 255;
+            this.friendSay.string = Dialogue[0];
+            this.node.on('touchend', function () {
+                if (i > 6) {
+                    cc.director.loadScene("Game");
+                } else if (i % 2 == 1) {
+                    this.friend.opacity = 0;
+                    this.me.opacity = 255;
+                    this.mySay.string = Dialogue[i]
+                } else if (i % 2 == 0) {
+                    this.friend.opacity = 255;
+                    this.me.opacity = 0;
+                    this.friendSay.string = Dialogue[i]
+                }
+
+                i++;
+            }, this)
+        } else {
+            Dialogue = this.text.json.rounds16.dialogue3;
+            this.gugu.opacity = 255;
+            this.guguSay.string = Dialogue[0];
+            this.node.on('touchend', function () {
+                if (i > 6) {
+                    cc.director.loadScene("Game");
+                } else if (i < 5) {
+                    if (i % 2 == 1) {
+                        this.gugu.opacity = 0;
+                        this.me.opacity = 255;
+                        this.mySay.string = Dialogue[i]
+                    } else if (i % 2 == 0) {
+                        this.gugu.opacity = 255;
+                        this.me.opacity = 0;
+                        this.guguSay.string = Dialogue[i]
+                    }
+                } else if (i == 5) {
+                    this.gugu.opacity = 0;
+                    this.friend.opacity = 255
+                    this.friendSay.string = Dialogue[i]
+                } else if (i == 6) {
+                    this.gugu.opacity = 255;
+                    this.friend.opacity = 0;
+                    this.guguSay.string = Dialogue[i]
+                } else if (i == 7) {
+                    this.gugu.opacity = 0;
+                    this.me.opacity = 255;
+                    this.mySay.string = Dialogue[i]
+                } else if (i == 8) {
+                    this.me.opacity = 0;
+                    this.friend.opacity = 255;
+                    this.friendSay.string = Dialogue[i]
+                }
+
+                i++;
+            }, this)
+        }
+    }
+});
---
+++ assets/JavaScript/Story24.js
@@ -0,0 +1,131 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        me:cc.Node,
+        friend: cc.Node,
+        xiaoer: cc.Node,
+        guizunan: cc.Node,
+        guizunv: cc.Node,
+        playerName: cc.Label,
+        friendSay:cc.Label,
+        mySay: cc.Label,
+        option: cc.Node,
+        op1: cc.Button,
+        op2: cc.Button,
+        text: cc.JsonAsset
+    },
+
+
+    onLoad () {
+        cc.director.preloadScene("Game");
+    },
+
+    start() {
+        this.option.active = false;
+        this.op1.interactable = false;
+        this.op2.interactable = false;
+        this.xiaoer.opacity = 0;
+        this.guizunan.opacity = 0;
+        this.guizunv.opacity = 0;
+        this.me.opacity = 0;
+        this.friend.opacity = 255;
+
+        var self = this;
+
+        var Dialogue = this.text.json.rounds24;
+        var i = 1;
+        this.node.on('touchend', function () {
+            if (i > 16) {
+                cc.director.loadScene("Game");
+            } else if (i < 8) {
+                if (i % 2 == 1) {
+                    self.friend.opacity = 0;
+                    self.me.opacity = 255;
+                    self.mySay.string = Dialogue[i]
+                } else if (i % 2 == 0) {
+                    self.friend.opacity = 255;
+                    self.me.opacity = 0;
+                    self.friendSay.string = Dialogue[i]
+                }
+            } else if (i == 8) {
+                self.xiaoer.opacity = 255;
+                self.me.opacity = 0;
+            } else if (i > 8 && i < 12) {
+                if (i % 2 == 0) {
+                    self.xiaoer.opacity = 0;
+                    self.friend.opacity = 0;
+                    self.me.opacity = 255;
+                    self.mySay.string = Dialogue[i]
+                } else if (i % 2 == 1) {
+                    self.xiaoer.opacity = 0;
+                    self.friend.opacity = 255;
+                    self.me.opacity = 0;
+                    self.friendSay.string = Dialogue[i]
+                }
+            } else if (i == 12) {
+                self.friend.opacity = 0;
+                self.me.opacity = 0;
+                self.guizunan.opacity = 255;
+            } else if (i == 13) {
+                self.guizunan.opacity = 0;
+                self.guizunv.opacity = 255;
+            } else if (i == 14) {
+                self.guizunv.opacity = 0;
+                self.friend.opacity = 255;
+                self.me.opacity = 0;
+                self.friendSay.string = Dialogue[i]
+            } else if (i == 15) {
+                self.option.active = true;
+                self.op1.interactable = true;
+                self.op2.interactable = true;
+                self.node.pauseSystemEvents(true);
+            } else if (i == 16) {
+                self.friend.opacity = 0;
+                self.me.opacity = 255;
+                self.mySay.string = Dialogue[i]
+            }
+
+            i++;
+        }, this)
+
+        var op1 = this.op1
+        var op2 = this.op2
+        var that = this;
+
+        this.op1.node.on("click", function () {
+            op1.interactable = false;
+            op2.interactable = false;
+            that.option.active = false;
+            that.continueDialogue1(that, true);
+        })
+
+        this.op2.node.on("click", function () {
+            op1.interactable = false;
+            op2.interactable = false;
+            that.option.active = false;
+            that.continueDialogue1(that, false);
+        })
+    },
+
+    continueDialogue1: function (self, flag) {
+        if (flag) {
+            //佛数值增加
+        } else {
+            //人+1
+        }
+
+        self.me.opacity = 0;
+        self.friend.opacity = 255;
+        self.friendSay.string = "那个三千万匹，是帛吗？这，这可是——";
+        console.log(self.friendSay.string);
+        self.node.resumeSystemEvents(true);
+    }
+});
